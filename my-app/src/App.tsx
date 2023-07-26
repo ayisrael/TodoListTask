6c40b9ba51d7233bc7dad567004abd9557557d5f
Normal_User <ayisrael@g.jct.il>
2023-07-26 10:17:55 +0300

    basic master

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,10 @@
 import { ApolloProvider } from "@apollo/client";
 import './App.css';
-import TodoList from './comps/todoList';
-import { apolloClient } from './service/apiService';
-
 
 
 function App() {
   return (
-    <ApolloProvider client={apolloClient}>
-      <TodoList/>
-    </ApolloProvider>
+    <></>
   );
 }
 
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-export type RootState = ReturnType<typeof store.getState>;
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
