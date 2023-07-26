dfe51897afd83d837985249349f359c2e86d25c9
Normal_User <ayisrael@g.jct.il>
2023-07-26 10:20:55 +0300

    master back

--- Todo-List-Backend/src/app.ts
+++ Todo-List-Backend/src/app.ts
@@ -1,23 +1,13 @@
 import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
-import { resolvers } from './gql/resolversTodo';
-import { Task } from './entity/todoTask';
-import { schema } from './gql/schemaTodo';
-
 dotenv.config();
 
 const app = express().disable('etag').disable('x-powered-by');
 
-const serverApollo = new ApolloServer({
-  typeDefs: schema,
-  resolvers,
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -30,29 +20,4 @@ if (IS_DEV) {
   app.use(cors());
 }
 
-serverApollo.applyMiddleware({
-  app,
-  cors: {
-    origin: 'http://localhost:3001',
-    credentials: true,
-  },
-});
-
-export const MyDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  schema: process.env.DB_SCHEMA,
-  entities: [Task],
-  synchronize: false,
-});
-
-MyDataSource.initialize()
-  .then(() => {
-    console.log('connected');
-  })
-  .catch(error => console.log(error));
 export { app };
